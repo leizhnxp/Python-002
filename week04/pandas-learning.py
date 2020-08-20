0c94bc583398d6f3f80d64ab350eab15bf2d078f
leizh <leizhenhua@arrail-dental.com>
2020-08-20 17:30:53 +0800

    按当周作业点评修改

--- week04/pandas-learning.py
+++ week04/pandas-learning.py
@@ -16,6 +16,12 @@ data = pd.DataFrame({
 
 # 返回所有数据就是直接引用DataFrame类型的变量就可以了
 # expr本来是为了输出的更好看点，不过最后放弃了，不如写点注释完事了，那天看pep8说特么的不让写中文注释么还是我理解错了
+## 这个作业有点评两点，一个是说可以写中文注释
+## 另一个说是复杂化了，这个就是充分说明点评者是不看注释那种，或者看不懂注释那种，expr就是为了把语句本身也输出出来，同时又不用写两遍
+## 前者当然我们知道是可以写中文注释的，要不这干嘛呢
+## 其实疑问来源是[这里](https://www.python.org/dev/peps/pep-0008/#source-file-encoding)，当时看这个觉得还是西方中心主义啊
+## 这次特别仔细看了一下，还有[这段](https://www.python.org/dev/peps/pep-0008/#comments)，专门的关于注释的段落，主要是国际化交流的考虑吧
+## 关于pep8，点评中写if __name__ == "__main__" 是pep8 风格？这个没有找到或看出有文本支持
 print('1. SELECT * FROM data;')
 expr = 'data'
 print(expr)
@@ -41,7 +47,14 @@ print('\n',"*"*20,'\n')
 # 选出一列其实返回的是个Series, 如果用双中括号（例如select多列），那返回还是个DataFrame，是二维结构，size不等同于表的记录数
 print('4. SELECT COUNT(id) FROM data')
 print(data['id'].size)
+print('\n',"*"*20,'\n')
 
+## 根据作业的点评提示，sql的count(*)和count(col)是不一样的，后者会排除掉null值
+## 那么，Series 的size是Series的槽位数吧，就不对了，而应改用count
+## 下面补充的示例，不知道怎么替换成python的None，所以替换成numpy的NaN
+
+print(data.id.replace(3,np.nan).count())
+print(data.id.replace(3,np.nan).size)
 print('\n',"*"*20,'\n')
 
 
@@ -52,6 +65,10 @@ print(data[(data['id'] < 1000 )& (data['age'] > 30)])
 
 print('\n',"*"*20,'\n')
 
+## 点评的作业里面，这块儿是这么写的，点评里面说效率是不如上面的写法的，这个我同意，不过，我觉得这哥们儿是真的懂了pandas的结构才能写成这样的
+print(df[df['id']<1000][df['age']>30])
+
+print('\n',"*"*20,'\n')
 
 # 这个上网搜了，参考的[这个](https://www.cnblogs.com/onemorepoint/p/10613953.html)，其实哪个方法我感觉都没看懂
 print('6. SELECT id,COUNT(DISTINCT order_id) FROM table1 GROUP BY id;')
@@ -66,6 +83,10 @@ print(table1.groupby(['id']).agg({'order_id': pd.Series.nunique}))
 
 print('\n',"*"*20,'\n')
 
+## 这个也是看了点评的作业，原来可以直接这么写，看语法上节省了agg，应该是一回事
+print(table1.groupby(['id'])['order_id'].nunique())
+## 推荐作业是这么写的
+print(table1.groupby('id').order_id.nunique())
 
 # 直接[参考](https://www.pypandas.cn/docs/getting_started/comparison.html#inner-join)
 print("7. SELECT * FROM table1 t1 INNER JOIN table2 t2 ON t1.id = t2.id;")
